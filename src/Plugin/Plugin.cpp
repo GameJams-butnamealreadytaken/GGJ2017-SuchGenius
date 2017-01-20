ab3d9dabfa217fb38e2dfd325a86184451585ac6
magestik <bastien@magestik.fr>
2017-01-20 21:14:54 +0100

    added baisc plugin

---
+++ src/Plugin/Plugin.cpp
@@ -0,0 +1,19 @@
+#include "Plugin.h"
+
+const CShIdentifier plugin_identifier("PluginGGJ2017");
+
+/**
+ * @brief Constructor
+ */
+PluginGGJ2017::PluginGGJ2017(void) : CShPlugin(plugin_identifier)
+{
+	// ...
+}
+
+/**
+ * @brief Destructor
+ */
+PluginGGJ2017::~PluginGGJ2017(void)
+{
+	// ...
+}
---
+++ src/Plugin/Plugin.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "ShEngineExt/ShEngineExt.h"
+
+class PluginGGJ2017 : public CShPlugin
+{
+public:
+
+	PluginGGJ2017(void);
+	virtual ~PluginGGJ2017(void);
+
+};
