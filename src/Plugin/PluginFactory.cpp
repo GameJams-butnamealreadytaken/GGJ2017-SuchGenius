3b329069f396d671f0dbf3af79b2409400d08dc2
magestik <bastien@magestik.fr>
2017-01-21 05:06:20 +0100

    added shockwave generation

--- src/Plugin/PluginFactory.cpp
+++ src/Plugin/PluginFactory.cpp
@@ -6,6 +6,21 @@
 
 static PluginGGJ2017 * g_pInstance = shNULL;
 
+static void OnTouchDown(int iTouch, float positionX, float positionY)
+{
+	g_pInstance->OnTouchDown(iTouch, positionX, positionY);
+}
+
+static void OnTouchUp(int iTouch, float positionX, float positionY)
+{
+	g_pInstance->OnTouchUp(iTouch, positionX, positionY);
+}
+
+static void OnTouchMove(int iTouch, float positionX, float positionY)
+{
+	g_pInstance->OnTouchMove(iTouch, positionX, positionY);
+}
+
 extern "C"
 {
 
@@ -14,6 +29,10 @@ void RegisterPluginGGJ2017(void)
 	if (!g_pInstance)
 	{
 		g_pInstance = new PluginGGJ2017();
+
+		ShInput::AddOnTouchDown(OnTouchDown);
+		ShInput::AddOnTouchUp(OnTouchUp);
+		ShInput::AddOnTouchMove(OnTouchMove);
 	}
 
 	ShApplication::RegisterPlugin(g_pInstance);
@@ -21,6 +40,10 @@ void RegisterPluginGGJ2017(void)
 
 void UnRegisterPluginGGJ2017(void)
 {
+	ShInput::RemoveOnTouchDown(OnTouchDown);
+	ShInput::RemoveOnTouchUp(OnTouchUp);
+	ShInput::RemoveOnTouchMove(OnTouchMove);
+
 	ShApplication::UnRegisterPlugin(g_pInstance);
 }
 
