0ac46cc1cae7c80a43e0c984e7453f4a20a2fb20
Thomas <tom.pradal@aliceadsl.fr>
2017-01-22 11:27:46 +0100

    Load last level played from start

--- src/Game/GameStateMainMenu.cpp
+++ src/Game/GameStateMainMenu.cpp
@@ -202,7 +202,7 @@ void GameStateMainMenu::update(float dt)
 		{
 			if (g_pInputs->IsPressEnter())
 			{
-				SetCurrentLevel(g_pGameSave->GetLastLevelPlayed());
+				SetCurrentLevel(g_pGameSave->GetLastLevelUnlocked());
 				Game & game = Game::instance();
 				game.push(Game::GAME_LEVEL);
 			}
@@ -328,7 +328,7 @@ void GameStateMainMenu::touchEnd(const CShVector2 & pos)
 
 			if (m_pPressedButton == m_pButtonPlay)
 			{
-				SetCurrentLevel(g_pGameSave->GetLastLevelPlayed());
+				SetCurrentLevel(g_pGameSave->GetLastLevelUnlocked());
 				game.push(Game::GAME_LEVEL);
 			}
 			else if (m_pPressedButton == m_pButtonSound)
